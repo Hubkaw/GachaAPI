33069ae7a5a2d44f01b140db1fa209cc60cf01f0
Hubkaw <2137insidejob@gmail.com>
2022-09-24 13:33:31 +0200

    Create Project

---
+++ src/main/java/com/gachaapi/GachaApiApplication.java
@@ -0,0 +1,13 @@
+package com.gachaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GachaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GachaApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gachaapi/GachaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gachaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GachaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
